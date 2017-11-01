e13cb6be00f3a750d74c30671c4a4aa18ae0ec74
Federico Ferri <federico.ferri.it@gmail.com>
2017-11-01 11:54:07 +0100

    implement spin() fn in Lua (specific for V-REP)

--- simExtBlueZero.lua
+++ simExtBlueZero.lua
@@ -1,10 +1,12 @@
 local simB0={}
 
---@fun example
---@arg int ui the ui handle
---@arg int widget the widget identifier
---@arg int index the index (0-based) where the new row will appear
-function simB0.example(ui,widget,index)
+--@fun spin
+--@arg string handle the node handle
+function simB0.spin(handle)
+    while sim.getSimulationState()~=sim.simulation_advancing_abouttostop do
+        simB0.spinOnce(handle)
+        sim.switchThread()
+    end
 end
 
 return simB0
